c46bab6824667f707f83c2ce1cb003bfb376bb10
derek.weber <derek.weber@NMC-CLG-2447>
2022-07-27 08:08:06 -0700

    Add project files.

---
+++ VinGenerator/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(149, 69)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(178, 49)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(454, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(191, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter an absolute path to .xlsx input file"
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 124)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(451, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(478, 170)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Work Order Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ VinGenerator/Form1.vb
@@ -0,0 +1,277 @@
+﻿Imports Microsoft.Office.Interop
+Public Class Form1
+    Private NumFiles
+    Private filesCreated = 0
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Main(TextBox1.Text)
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        MsgBox("Done")
+        Button1.Enabled = True
+        Application.Exit()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Enabled = False
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub SubAssembly(prefix As String, vins As List(Of String), dates As List(Of String), fileName As String)
+        Dim xlsapp As Excel.Application
+        Dim xlsworkbook As Excel.Workbook
+        Dim xlsworksheet As Excel.Worksheet
+        xlsapp = New Excel.Application
+        xlsworkbook = xlsapp.Workbooks.Open("c:\users\derek.weber\source\repos\workorder\workorder\work order templates\" + fileName)
+        xlsworksheet = xlsworkbook.Worksheets(2)
+        Dim numRows = xlsworksheet.UsedRange.Rows.Count - 1
+        Dim currentRow = 2
+        Dim xlscell As Excel.Range
+
+        Dim PSN As String
+        Dim WOPROD As String
+        Dim SOPROD As String
+        Dim temp As String
+        Dim DirectoryPathFolder As String
+        DirectoryPathFolder = "C:\Users\derek.weber\source\repos\WorkOrder\WorkOrder\Subassemblies\"
+        Dim DirectoryPathFile As String
+        For i As Integer = 0 To vins.Count() - 1
+            xlsworksheet = xlsworkbook.Worksheets(1)
+            Dim ch15 As Char = vins.ElementAt(i)(14)
+            Dim ch16 As Char = vins.ElementAt(i)(15)
+            Dim ch17 As Char = vins.ElementAt(i)(16)
+            Dim row As String = (i + 2).ToString()
+            PSN = ch15 + ch16 + ch17
+
+            WOPROD = "WO-PROD-" + PSN
+            SOPROD = "SO-" + PSN
+            'move variables and assign to cells
+            xlscell = xlsworksheet.Range("a" + row)
+            xlscell.Value = prefix + WOPROD
+            xlscell = xlsworksheet.Range("b" + row)
+            xlscell.Value = prefix + vins.ElementAt(i)
+            xlscell = xlsworksheet.Range("c2")
+            temp = xlscell.Value
+            xlscell = xlsworksheet.Range("c" + row)
+            xlscell.Value = temp
+            xlscell = xlsworksheet.Range("d2")
+            temp = xlscell.Value
+            xlscell = xlsworksheet.Range("d" + row)
+            xlscell.Value = temp
+            xlscell = xlsworksheet.Range("e2")
+            temp = xlscell.Value
+            xlscell = xlsworksheet.Range("e" + row)
+            xlscell.Value = temp
+            xlscell = xlsworksheet.Range("f" + row)
+            xlscell.Value = dates.ElementAt(i)
+            xlscell = xlsworksheet.Range("g2")
+            temp = xlscell.Value
+            xlscell = xlsworksheet.Range("g" + row)
+            xlscell.Value = temp
+
+            For j As Integer = 2 To numRows + 1
+                xlsworksheet = xlsworkbook.Worksheets(2)
+                xlscell = xlsworksheet.Range("a" + currentRow.ToString())
+                xlscell.Value = prefix + vins.ElementAt(i)
+                xlscell = xlsworksheet.Range("b" + j.ToString())
+                temp = xlscell.Value
+                xlscell = xlsworksheet.Range("b" + currentRow.ToString())
+                xlscell.Value = temp
+                xlscell = xlsworksheet.Range("c" + j.ToString())
+                temp = xlscell.Value
+                xlscell = xlsworksheet.Range("c" + currentRow.ToString())
+                xlscell.Value = temp
+                xlscell = xlsworksheet.Range("d" + j.ToString())
+                temp = xlscell.Value
+                xlscell = xlsworksheet.Range("d" + currentRow.ToString())
+                xlscell.Value = temp
+                xlscell = xlsworksheet.Range("e" + j.ToString())
+                temp = xlscell.Value
+                xlscell = xlsworksheet.Range("e" + currentRow.ToString())
+                xlscell.Value = temp
+                xlscell = xlsworksheet.Range("h" + j.ToString())
+                temp = xlscell.Value
+                xlscell = xlsworksheet.Range("h" + currentRow.ToString())
+                xlscell.Value = temp
+                xlscell = xlsworksheet.Range("i" + j.ToString())
+                temp = xlscell.Value
+                xlscell = xlsworksheet.Range("i" + currentRow.ToString())
+                xlscell.Value = temp
+                xlscell = xlsworksheet.Range("j" + j.ToString())
+                temp = xlscell.Value
+                xlscell = xlsworksheet.Range("j" + currentRow.ToString())
+                xlscell.Value = temp
+                xlscell = xlsworksheet.Range("k" + j.ToString())
+                temp = xlscell.Value
+                xlscell = xlsworksheet.Range("k" + currentRow.ToString())
+                xlscell.Value = temp
+                currentRow = currentRow + 1
+            Next
+            ''last save and close
+            filesCreated = filesCreated + 1
+            BackgroundWorker1.ReportProgress((filesCreated * 100) / NumFiles)
+
+        Next
+        DirectoryPathFile = DirectoryPathFolder + prefix
+        xlsworkbook.SaveAs(DirectoryPathFile)
+        xlsworkbook.Close()
+
+    End Sub
+
+    Sub VinWorkOrder(vins As List(Of String), dates As List(Of String))
+        Dim PSN As String
+        Dim WOPROD As String
+        Dim SOPROD As String
+        Dim DirectoryPathFolder As String
+        For i As Integer = 0 To vins.Count() - 1
+            Dim ch15 As Char = vins.ElementAt(i)(14)
+            Dim ch16 As Char = vins.ElementAt(i)(15)
+            Dim ch17 As Char = vins.ElementAt(i)(16)
+            PSN = ch15 + ch16 + ch17
+
+            WOPROD = "WO-PROD-" + PSN
+            SOPROD = "SO-" + PSN
+            Dim xlsApp As Excel.Application
+            Dim xlsWorkBook As Excel.Workbook
+            Dim xlsWorkSheet As Excel.Worksheet
+            Dim xlsCell As Excel.Range
+
+            ' Initialise Excel Object
+            xlsApp = New Excel.Application
+
+            ' Open test Excel spreadsheet
+            xlsWorkBook = xlsApp.Workbooks.Open("C:\Users\derek.weber\source\repos\WorkOrder\WorkOrder\\WORK ORDER TEMPLATES\MAIN BUILD-WORK ORDER.xlsx")
+            ' Open worksheet 
+            xlsWorkSheet = xlsWorkBook.Worksheets(1)
+            '
+
+            'Move Variables and Assign to Cells
+            xlsCell = xlsWorkSheet.Range("A2")
+            xlsCell.Value = WOPROD
+            xlsCell = xlsWorkSheet.Range("B2")
+            xlsCell.Value = vins.ElementAt(i)
+            xlsCell = xlsWorkSheet.Range("F2")
+            xlsCell.Value = dates.ElementAt(i)
+            xlsCell = xlsWorkSheet.Range("H2")
+            xlsCell.Value = SOPROD
+            xlsCell = xlsWorkSheet.Range("J2")
+            xlsCell.Value = PSN
+
+            'LAST ROW 
+
+            xlsWorkSheet = xlsWorkBook.Worksheets(2)
+            Dim lastRow As Integer = xlsWorkSheet.UsedRange.Rows.Count
+            'This is next emptyRow in the sheet
+            Dim emptyRow As Integer = lastRow + 1
+
+            Dim lastRow1 As String
+            lastRow1 = lastRow
+            Dim lastcell As String
+            lastcell = "A" + lastRow1
+            Dim newrange1 As String
+            newrange1 = "A2:" + lastcell
+            'MessageBox.Show(newrange1)
+            xlsCell = xlsWorkSheet.Range(newrange1)
+            xlsCell.Value = vins.ElementAt(i)
+
+            'CASES AND JOIN DB
+            Dim DirectoryPathFile As String
+            DirectoryPathFolder = "C:\Users\derek.weber\source\repos\WorkOrder\WorkOrder\"
+            My.Computer.FileSystem.CreateDirectory(DirectoryPathFolder)
+            'Last Save and Close
+            DirectoryPathFile = "C:\Users\derek.weber\source\repos\WorkOrder\WorkOrder\" + vins.ElementAt(i)
+            xlsWorkBook.SaveAs(DirectoryPathFile)
+            xlsWorkBook.Close()
+            filesCreated = filesCreated + 1
+            BackgroundWorker1.ReportProgress((filesCreated * 100) / NumFiles)
+        Next
+        DirectoryPathFolder = "C:\Users\derek.weber\source\repos\WorkOrder\WorkOrder\" + "\Subassemblies\"
+        My.Computer.FileSystem.CreateDirectory(DirectoryPathFolder)
+
+
+    End Sub
+
+    Function VinIsValid(vin As String) As Boolean
+        If (vin.Length <> 17) Then
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Sub Main(path As String)
+        Dim xlsApp As Excel.Application
+        Dim xlsWorkBook As Excel.Workbook
+        Dim xlsWorkSheet As Excel.Worksheet
+        Dim vins As New List(Of String)
+        Dim dates As New List(Of String)
+
+        Try
+            xlsApp = New Excel.Application
+            'xlsWorkBook = xlsApp.Workbooks.Open("C:\Users\derek.weber\source\repos\WorkOrder\WorkOrder\input.xlsx")
+            xlsWorkBook = xlsApp.Workbooks.Open(path)
+            xlsWorkSheet = xlsWorkBook.Worksheets("sheet1")
+
+            Dim Range = xlsWorkSheet.UsedRange
+            NumFiles = Range.Rows.Count * 22
+
+            For rcnt = 1 To Range.Rows.Count
+                Dim obj = CType(Range.Cells(rcnt, 1), Excel.Range)
+                Dim obj1 = CType(Range.Cells(rcnt, 2), Excel.Range)
+                If (VinIsValid(obj.Value) And IsDate(obj1.Value)) Then
+                    vins.Add(obj.Value)
+                    dates.Add(obj1.Value)
+                End If
+            Next
+
+            xlsWorkBook.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+            Application.Exit()
+        Finally
+
+        End Try
+        If (vins.Count = 0) Then
+            MsgBox("Either the input file was empty or there were no valid vin date combinations")
+            Application.Exit()
+        End If
+        Try
+            VinWorkOrder(vins, dates)
+            SubAssembly("5THWHEEL-", vins, dates, "5THWHEEL - WORK ORDER.xlsx")
+            SubAssembly("ACC-", vins, dates, "AC COMPRESSOR - WORK ORDER.xlsx")
+            SubAssembly("AIRT-", vins, dates, "AIR TANK - WORK ORDER.xlsx")
+            SubAssembly("PREX-", vins, dates, "BATTERY PACK - WORK ORDER.xlsx")
+            SubAssembly("BUMP-", vins, dates, "BUMPER - WORK ORDER.xlsx")
+            SubAssembly("CABP-", vins, dates, "CAB PREP - WORK ORDER.xlsx")
+            SubAssembly("COMP-", vins, dates, "COMPRESSOR-HEATER - WORK ORDER.xlsx")
+            SubAssembly("DCDC-", vins, dates, "DCDC - WORK ORDER.xlsx")
+            SubAssembly("EAXLE-", vins, dates, "E-AXLE - WORK ORDER.xlsx")
+            SubAssembly("EXP-", vins, dates, "EXPANSION TANK - WORK ORDER.xlsx")
+            SubAssembly("FBEAM-", vins, dates, "FRONT BEAM SUSPENSION - WORK ORDER.xlsx")
+            SubAssembly("FAXLE-", vins, dates, "FRONT-AXLE - WORK ORDER.xlsx")
+            SubAssembly("FUP-", vins, dates, "FUP - WORK ORDER.xlsx")
+            SubAssembly("HVH-", vins, dates, "HV HEATER - WORK ORDER.xlsx")
+            SubAssembly("INV-", vins, dates, "INVERTER - WORK ORDER.xlsx")
+            SubAssembly("LVBAT-", vins, dates, "LV BATTERY BOX - WORK ORDER.xlsx")
+            SubAssembly("PNEL-", vins, dates, "PNEUMATIC LINES - WORK ORDER.xlsx")
+            SubAssembly("PNEV-", vins, dates, "PNEUMATIC VALVES - WORK ORDER.xlsx")
+            SubAssembly("TAIL-", vins, dates, "TAILLIGHT - WORK ORDER.xlsx")
+            SubAssembly("TAXLE-", vins, dates, "TAG AXLE - WORK ORDER.xlsx")
+            SubAssembly("TCOIL-", vins, dates, "TRAILER COIL - WORK ORDER.xlsx")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+            Application.Exit()
+        Finally
+
+        End Try
+    End Sub
+
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+End Class
